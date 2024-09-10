decf44666092b55a83a8e06700ea36d16e48d01b
Daniela Garcia <danielagarcia13.99@gmail.com>
2024-09-10 04:10:19 +0000

    rake grade issue

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+require "json"
 
 
 get("/") do
   access_exchange_key= ENV.fetch("EXCHANGE_RATE_KEY")
   currencies_url= HTTP.get("https://api.exchangerate.host/list?access_key=#{access_exchange_key}")
-  currency_data=JSON.parse(currencies_url)
+  currency_data=JSON.parse(currencies_url.to_s)
   currencies_hash=currency_data.fetch("currencies")
   @list_of_currencies=currencies_hash.keys
 
@@ -16,7 +17,7 @@ end
 get("/:currency") do
   access_exchange_key= ENV.fetch("EXCHANGE_RATE_KEY") 
   currencies_url= HTTP.get("https://api.exchangerate.host/list?access_key=#{access_exchange_key}")
-  currency_data=JSON.parse(currencies_url)
+  currency_data=JSON.parse(currencies_url.to_s)
   currencies_hash=currency_data.fetch("currencies")
   @list_of_currencies=currencies_hash.keys
 
@@ -29,7 +30,7 @@ get("/:currency/:other_currency") do
 @second_currency = params.fetch("other_currency")
 access_exchange_key= ENV.fetch("EXCHANGE_RATE_KEY") 
 conversion_url= HTTP.get("https://api.exchangerate.host/convert?from=#{@first_currency}&to=#{@second_currency}&amount=1&access_key=#{access_exchange_key}")
-conversion_data=JSON.parse(conversion_url)
+conversion_data=JSON.parse(conversion_url.to_s)
 @conversion_amount=conversion_data.fetch("result")
 erb(:conversion_page)
 
